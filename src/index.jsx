68652e167fa89a0eb8d63848def0d84eff437a7a
skyrbe <harsha.venkatram@gmail.com>
2020-06-21 22:31:31 +0530

    Added animations

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,9 @@
+/* eslint-disable */
 import React, { useState } from 'react';
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 import './styles.css';
 
-const Container = styled.div(_ => ({
-  position: 'relative'
-}));
-
-const Image = styled.div(props => ({
+const Image = styled.div((props) => ({
   width: props.width || '400px',
   height: props.height || '300px',
   borderRadius: props.borderRadius || '5px',
@@ -14,20 +11,25 @@ const Image = styled.div(props => ({
   backgroundPosition: 'center',
   backgroundSize: 'cover',
   cursor: 'pointer',
-  boxShadow: '0 36px 10px -20px rgba(0, 0, 0, 0.2)',
+  boxShadow: props.boxShadow || '0 20px 10px -10px rgba(0, 0, 0, 0.2)',
   position: 'absolute',
   top: '0',
   left: '0',
-  zIndex: 1
+  zIndex: 1,
+  transition: 'transform 0.3s ease-in'
 }));
 
-const Info = styled.div(props => ({
+const Info = styled.div((props) => ({
   width: props.width || '400px',
   height: props.height || '300px',
   borderRadius: props.borderRadius || '5px',
+  padding: '10px',
+  paddingTop: parseInt(props.height) - 70 + 'px' || '130px',
   position: 'absolute',
   top: '0',
-  left: '0'
+  left: '0',
+  backgroundColor: 'white',
+  transition: 'transform 0.3s ease-in'
 }));
 
 const InfoCard = ({
@@ -35,19 +37,23 @@ const InfoCard = ({
   styles
 }) => {
 
-  const [isExpanded, setExpanded] = useState(true);
+  const [isExpanded, setExpanded] = useState(false);
 
   const toggleExpanded = () => {
     setExpanded(!isExpanded);
   };
 
+  let classnames = 'react-info-card__container';
+  if(isExpanded) {
+    classnames = `${classnames} react-info-card__slide`;
+  }
   return (
-    <Container>
-      <Image {...styles} onClick={toggleExpanded} />
-      <Info {...styles}>
+    <div className={classnames} onClick={toggleExpanded}>
+      <Image {...styles} className='react-info-card__image' />
+      <Info {...styles} className='react-info-card__body'>
         {children}
       </Info>
-    </Container>
+    </div>
   );
 };
 
